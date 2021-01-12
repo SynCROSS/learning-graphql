01f473873b2758ada945f52321e9671551047b86
SynCROSS <51585533+yuto3125@users.noreply.github.com>
2021-01-12 19:47:17 +0900

    Slight Modification

--- graphql/database.js
+++ graphql/database.js
@@ -1,10 +1,10 @@
 // import fetch from 'node-fetch';
 import axios from 'axios';
 
-const BASE_URL = 'https://yts.mx/api/v2/';
-const LIST_MOVIE_URL = `${BASE_URL}list_movies.json`;
-const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
-const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
+const BASE_URL = 'https://yts.mx/api/v2';
+const LIST_MOVIE_URL = `${BASE_URL}/list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}/movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}/movie_suggestions.json`;
 
 export const getMovies = async (
   limit,
